d5ab72361e4d434bee0f98c9e745f118dd52b5a2
Y-Funakoshi <ykltk18@gmail.com>
2015-12-26 19:14:36 +0900

    復活のf

---
+++ search/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+'''
+class Search(models.Model):
+	your_name = models.CharField(max_length=10)
+	
+	def __str__(self):
+		return "<{0}>".format(self.your_name)	
+'''
---
+++ search/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url, include
+#from labsw.views import search
+from search import views
+#from . import views
+#from search.views import *
+#from download import views
+
+urlpatterns = patterns('',
+	#url(r'^search/', views.search, name='search'),
+	url(r'^$', views.search, name='search'),
+	url(r'^edit/(?P<editing_id>\d+)/$', views.edit, name='edit'),
+	url(r'^delete/$', views.delete, name='delete'),
+	url(r'^search/(?P<editing_id>\d+)/$', views.download, name='download'),
+	#url(r'^search/kekka/(?P<book_id>\d+)/$', views.show, name='show'),
+)
