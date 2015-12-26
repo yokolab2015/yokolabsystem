eced318a1403a8d85213c14d43f74bdc734aeb42
sttr110 <mydogs@live.jp>
2015-12-26 15:58:42 +0000

    destroy file

--- docmng/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from docmng.models import Userinfo 
-from docmng.models import Document 
-
-admin.site.register(Userinfo)
-admin.site.register(Document)
-# Register your models here.
--- search/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from docmng.models import Document
-
-#class EntryAdmin(admin.ModelAdmin):
-#	list_display = ('id', 'title')
-#	admin.site.register(Document, EntryAdmin)
-# Register your models here.
