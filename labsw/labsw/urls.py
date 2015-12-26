6831ec58b22effe10ed69d09f1af0bc52dc26127
sttr110 <mydogs@live.jp>
2015-12-26 19:59:05 +0000

    auth画面とかのテンプレート作成、accountsはまだない

--- labsw/labsw/urls.py
+++ labsw/labsw/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     #authentications
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}),
+        {'template_name': 'authentications/login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'template_name': 'logged_out.html'}),
+        {'template_name': 'authentications/logged_out.html'}),
 ]
